08973e4b925c7c544ca83f104cf67824106c5242
Rubens-prog <rubensarjunior@gmail.com>
2021-06-29 18:49:04 -0300

    adding pics

--- main.js
+++ main.js
@@ -30,3 +30,14 @@ for (const link of links) {
     nav.classList.remove('show');
   });
 }
+/*================== Scroll ====================*/
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > navHeight) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+});
