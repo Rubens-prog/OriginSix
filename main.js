047714cba5f131f2ef6bf59673e40af148c7d0a5
Rubens-prog <rubensarjunior@gmail.com>
2021-07-07 09:11:09 -0300

    fixing cards

--- main.js
+++ main.js
@@ -31,11 +31,10 @@ for (const link of links) {
   });
 }
 /*================== Scroll ====================*/
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header');
-  const navHeight = header.offsetHeight;
-
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
   } else {
@@ -82,14 +81,38 @@ scrollReveal.reveal(
 );
 
 /*================== BACK TO TOP ====================*/
+const backToTopButton = document.querySelector('.back-to-top');
 
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top');
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show');
   } else {
     backToTopButton.classList.remove('show');
+  }
+}
+
+/*================== Menu Ativo Conforme a Section  ====================*/
+const sections = document.querySelectorAll('main section[id]');
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYoffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a.title[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('nav ul li a.title[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
   }
 }
 
@@ -98,4 +121,5 @@ function backToTop() {
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
